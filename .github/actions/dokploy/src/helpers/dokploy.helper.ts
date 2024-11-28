1c19eb43e8c32187030a4c09b2aba9a66c744cf9
pchmn <pchmn.dev@gmail.com>
2024-11-28 11:17:46 +0100

    ci: refactor dokploy action

---
+++ .github/actions/dokploy/src/helpers/dokploy.helper.ts
@@ -0,0 +1,117 @@
+import * as core from "@actions/core";
+import {
+  applicationCreate,
+  applicationOne,
+  domainByApplicationId,
+  domainCreate,
+  projectOne,
+  serverPublicIp,
+} from "../sdk/client";
+import type { Application, Domain, Project } from "../sdk/types";
+import { toKebabCase } from "../utils/string";
+
+export async function getOrCreateApplication({
+  applicationId,
+  applicationName,
+  projectId,
+  dockerPort,
+}: {
+  applicationId?: string;
+  applicationName: string;
+  projectId: string;
+  dockerPort: string;
+}) {
+  if (applicationId) {
+    const { data } = await applicationOne({
+      query: { applicationId },
+    });
+
+    if (data) {
+      core.info(`Application ${applicationName} found`);
+      return data as Application;
+    }
+  }
+
+  const { data: project } = await projectOne({
+    query: { projectId },
+  });
+  for (const application of (project as Project | undefined)?.applications ??
+    []) {
+    if (application.name === applicationName) {
+      core.info(`Application ${applicationName} found`);
+      return application;
+    }
+  }
+
+  core.info(`Creating application ${applicationName}...`);
+  const { data: application } = await applicationCreate({
+    body: {
+      projectId,
+      name: applicationName,
+      appName: applicationName,
+    },
+  });
+  if (application) {
+    const domain = await generateDomain({
+      appName: (application as Application).name,
+      projectName: (project as Project).name,
+    });
+
+    await domainCreate({
+      body: {
+        host: domain,
+        applicationId: (application as Application).applicationId,
+        https: true,
+        certificateType: "letsencrypt",
+        port: Number.parseInt(dockerPort ?? "80"),
+      },
+    });
+    return application as Application;
+  }
+}
+
+async function generateDomain({
+  appName,
+  projectName,
+  domain = "sslip.io",
+}: {
+  appName: string;
+  projectName: string;
+  domain?: string;
+}) {
+  const subdomain = toKebabCase(`${projectName}-${appName}`);
+
+  if (domain === "sslip.io") {
+    const { data: publicIp } = await serverPublicIp();
+    return `${subdomain}.${(publicIp as string).replace(/\./g, "-")}.${domain}`;
+  }
+
+  return `${subdomain}.${domain}`;
+}
+
+export async function waitForDeploymentToBeDone(applicationId: string) {
+  const res = await applicationOne({
+    query: { applicationId },
+  });
+  const application = res.data as Application | undefined;
+  if (!application) {
+    throw new Error("Application not found");
+  }
+  if (application.applicationStatus === "error") {
+    throw new Error("Application deployment failed");
+  }
+  if (application.applicationStatus === "done") {
+    return;
+  }
+
+  // wait for 5 seconds before checking again
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await waitForDeploymentToBeDone(applicationId);
+}
+
+export async function getApplicationUrl(applicationId: string) {
+  const { data } = await domainByApplicationId({
+    query: { applicationId },
+  });
+  return `https://${(data as Domain[])[0].host}`;
+}
--- .github/actions/dokploy/src/index.ts
+++ .github/actions/dokploy/src/index.ts
@@ -3,18 +3,16 @@ import * as github from "@actions/github";
 import { getConfig } from "./config";
 import { setPullRequestComment } from "./github/comment-pr";
 import {
-  applicationCreate,
-  applicationOne,
+  getApplicationUrl,
+  getOrCreateApplication,
+  waitForDeploymentToBeDone,
+} from "./helpers/dokploy.helper";
+import {
+  applicationDeploy,
   applicationSaveDockerProvider,
   applicationSaveEnvironment,
   client,
-  domainByApplicationId,
-  domainCreate,
-  projectOne,
-  serverPublicIp,
 } from "./sdk/client";
-import type { Application, Domain, Project } from "./sdk/types";
-import { toKebabCase } from "./utils/string";
 
 const config = getConfig();
 let octokit: ReturnType<typeof github.getOctokit>;
@@ -27,18 +25,15 @@ client.setConfig({
     Authorization: `Bearer ${config.dokployToken}`,
   },
 });
-client.interceptors.request.use((request, options) => {
-  core.debug(`Request: ${JSON.stringify(request, null, 2)}`);
-  return request;
-});
-client.interceptors.response.use((response, request, options) => {
-  core.debug(`Response: ${JSON.stringify(response, null, 2)}`);
-  return response;
-});
 
 async function main() {
   try {
-    const application = await getOrCreateApplication();
+    const application = await getOrCreateApplication({
+      applicationId: config.applicationId,
+      applicationName: config.applicationName,
+      projectId: config.projectId,
+      dockerPort: config.dockerPort,
+    });
     core.debug(`Application: ${JSON.stringify(application, null, 2)}`);
 
     if (application) {
@@ -61,7 +56,10 @@ async function main() {
       });
 
       core.info(`Deploying ${application.name}...`);
-      await deployApplication(application.applicationId);
+      await applicationDeploy({
+        parseAs: "text",
+        body: { applicationId: application.applicationId },
+      });
 
       core.debug("Waiting for deployment to be done...");
       await waitForDeploymentToBeDone(application.applicationId);
@@ -84,126 +82,6 @@ async function main() {
     core.error(error.stack);
     core.setFailed(error.message);
   }
-}
-
-async function getOrCreateApplication() {
-  if (config.applicationId) {
-    const { data } = await applicationOne({
-      query: { applicationId: config.applicationId },
-    });
-
-    if (data) {
-      core.info(`Application ${config.applicationName} found`);
-      return data as Application;
-    }
-  }
-
-  const { data: project } = await projectOne({
-    query: { projectId: config.projectId },
-  });
-  for (const application of (project as Project | undefined)?.applications ??
-    []) {
-    if (application.name === config.applicationName) {
-      core.info(`Application ${config.applicationName} found`);
-      return application;
-    }
-  }
-
-  core.info(`Creating application ${config.applicationName}...`);
-  const { data: application } = await applicationCreate({
-    body: {
-      projectId: config.projectId,
-      name: config.applicationName,
-      appName: config.applicationName,
-    },
-  });
-  if (application) {
-    // const { data: host } = await domainGenerateDomain({
-    //   body: {
-    //     appName: config.applicationName,
-    //   },
-    // });
-
-    const domain = await generateDomain({
-      appName: config.applicationName,
-      projectName: (project as Project).name,
-    });
-
-    await domainCreate({
-      body: {
-        host: domain,
-        applicationId: (application as Application).applicationId,
-        https: true,
-        certificateType: "letsencrypt",
-        port: Number.parseInt(config.dockerPort ?? "80"),
-      },
-    });
-    return application as Application;
-  }
-}
-
-async function getApplicationUrl(applicationId: string) {
-  const { data } = await domainByApplicationId({
-    query: { applicationId },
-  });
-  return `https://${(data as Domain[])[0].host}`;
-}
-
-async function generateDomain({
-  appName,
-  projectName,
-  domain = "sslip.io",
-}: {
-  appName: string;
-  projectName: string;
-  domain?: string;
-}) {
-  const subdomain = toKebabCase(`${projectName}-${appName}`);
-
-  if (domain === "sslip.io") {
-    const { data: publicIp } = await serverPublicIp();
-    return `${subdomain}.${(publicIp as string).replace(/\./g, "-")}.${domain}`;
-  }
-
-  return `${subdomain}.${domain}`;
-}
-
-async function deployApplication(applicationId: string) {
-  core.debug(`Deploying application ${JSON.stringify({ applicationId })}...`);
-  const res = await fetch(`${config.dokployBaseUrl}/api/application.deploy`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${config.dokployToken}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ applicationId }),
-  });
-
-  if (!res.ok) {
-    return { error: await res.json() };
-  }
-
-  return { error: undefined };
-}
-
-async function waitForDeploymentToBeDone(applicationId: string) {
-  const res = await applicationOne({
-    query: { applicationId },
-  });
-  const application = res.data as Application | undefined;
-  if (!application) {
-    throw new Error("Application not found");
-  }
-  if (application.applicationStatus === "error") {
-    throw new Error("Application deployment failed");
-  }
-  if (application.applicationStatus === "done") {
-    return;
-  }
-
-  // wait for 5 seconds before checking again
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  await waitForDeploymentToBeDone(applicationId);
 }
 
 main();
