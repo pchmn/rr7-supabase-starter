0bed38de1ecede510b7f5c359c9405f851d286cf
pchmn <pchmn.dev@gmail.com>
2024-12-19 19:59:58 +0100

    lint: biome format

--- packages/ssr-theme/src/useBroadcastChannel.ts
+++ packages/ssr-theme/src/useBroadcastChannel.ts
@@ -1,5 +1,5 @@
-import {useEffect, useCallback, useRef} from 'react'
-import type {MutableRefObject} from 'react'
+import { useEffect, useCallback, useRef } from 'react';
+import type { MutableRefObject } from 'react';
 
 export function useBroadcastChannel<T = string>(
   channelName: string,
@@ -10,17 +10,17 @@ export function useBroadcastChannel<T = string>(
     typeof window !== 'undefined' && 'BroadcastChannel' in window
       ? new BroadcastChannel(channelName + '-channel')
       : null,
-  )
+  );
 
-  useChannelEventListener(channelRef, 'message', handleMessage)
-  useChannelEventListener(channelRef, 'messageerror', handleMessageError)
+  useChannelEventListener(channelRef, 'message', handleMessage);
+  useChannelEventListener(channelRef, 'messageerror', handleMessageError);
 
   return useCallback(
     (data: T) => {
-      channelRef?.current?.postMessage(data)
+      channelRef?.current?.postMessage(data);
     },
     [channelRef],
-  )
+  );
 }
 
 function useChannelEventListener<K extends keyof BroadcastChannelEventMap>(
@@ -29,10 +29,10 @@ function useChannelEventListener<K extends keyof BroadcastChannelEventMap>(
   handler: (e: BroadcastChannelEventMap[K]) => void = () => {},
 ) {
   useEffect(() => {
-    const channel = channelRef.current
+    const channel = channelRef.current;
     if (channel) {
-      channel.addEventListener(event, handler)
-      return () => channel.removeEventListener(event, handler)
+      channel.addEventListener(event, handler);
+      return () => channel.removeEventListener(event, handler);
     }
-  }, [channelRef, event, handler])
+  }, [channelRef, event, handler]);
 }
--- packages/ssr-theme/src/useCorrectCssTransition.ts
+++ packages/ssr-theme/src/useCorrectCssTransition.ts
@@ -1,7 +1,7 @@
-import {useCallback} from 'react'
+import { useCallback } from 'react';
 
 function withoutTransition(callback: Function) {
-  const css = document.createElement('style')
+  const css = document.createElement('style');
   css.appendChild(
     document.createTextNode(
       `* {
@@ -12,32 +12,32 @@ function withoutTransition(callback: Function) {
        transition: none !important;
     }`,
     ),
-  )
-  document.head.appendChild(css)
+  );
+  document.head.appendChild(css);
 
-  callback()
+  callback();
 
   setTimeout(() => {
     // Calling getComputedStyle forces the browser to redraw
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const _ = window.getComputedStyle(css).transition
-    document.head.removeChild(css)
-  }, 0)
+    const _ = window.getComputedStyle(css).transition;
+    document.head.removeChild(css);
+  }, 0);
 }
 
 export function useCorrectCssTransition({
   disableTransitions = false,
-}: {disableTransitions?: boolean} = {}) {
+}: { disableTransitions?: boolean } = {}) {
   return useCallback(
     (callback: Function) => {
       if (disableTransitions) {
         withoutTransition(() => {
-          callback()
-        })
+          callback();
+        });
       } else {
-        callback()
+        callback();
       }
     },
     [disableTransitions],
-  )
+  );
 }
