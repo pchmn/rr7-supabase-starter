b149a9791b757c73dae62f7883dfc6c750ba0cc4
pchmn <pchmn.dev@gmail.com>
2025-02-18 17:45:34 +0100

    feat: upgrade shadcn an tailwind

--- apps/web/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import baseConfig from '@rr7-supabase-starter/ui/tailwind.config';
-import type { Config } from 'tailwindcss';
-
-export default {
-  content: ['./app/**/*.{ts,tsx}', '../../packages/ui/src/**/*.{ts,tsx}'],
-  presets: [baseConfig],
-} satisfies Config;
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,17 +1,16 @@
 import { reactRouter } from '@react-router/dev/vite';
-import autoprefixer from 'autoprefixer';
+import tailwindcss from '@tailwindcss/vite';
 import { reactRouterHonoServer } from 'react-router-hono-server/dev';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), reactRouterHonoServer(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    reactRouterHonoServer(),
+    tsconfigPaths(),
+    tailwindcss(),
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(process.env.APP_VERSION || '0.0.1'),
     __APP_VERSION_DATE__: JSON.stringify(
